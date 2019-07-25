88cb938bccc482834ed55f692a400c679053828e
pakhomova <evgpakhomova@gmail.com>
2019-07-25 14:02:20 +0300

    Update post.js

--- src/containers/latest-posts-page/components/post.js
+++ src/containers/latest-posts-page/components/post.js
@@ -3,11 +3,17 @@ import { Link } from 'react-router-dom';
 import routes from '../../../constants/routes.json';
 
 export default class Post extends Component {
+  getShortText = text => {
+    if (text.length > 130) {
+      return text.slice(0, 131) + '...';
+    }
+    return text;
+  };
   render() {
     return (
       <div className="item">
         <Link to={routes.POSTS + '/' + this.props.id}>{this.props.title}</Link>
-        <p>{this.props.body}</p>
+        <p>{this.getShortText(this.props.body)}</p>
       </div>
     );
   }
